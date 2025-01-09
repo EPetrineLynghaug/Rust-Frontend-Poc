f91921adcb886e57e0b3bbf2e14973f92b226e83
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-01-09 18:50:05 +0100

    styling funker.

--- user_manager_frontend/src/main.rs
+++ user_manager_frontend/src/main.rs
@@ -1,5 +1,5 @@
 use crate::components::App;
-use dotenv::dotenv;
+
 use yew::Renderer;
 
 mod components;
@@ -7,6 +7,6 @@ mod user_manager;
 
 fn main() {
     // Last inn miljøvariabler fra .env-filen
-    dotenv().ok();
+
     Renderer::<App>::new().render();
 }
